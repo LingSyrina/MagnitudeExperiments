eac47157a2eb4d35d8c371eaece565168c4e9390
Ling Sun <ls44@iu.edu>
2025-06-16 16:39:49 -0400

    Update slider_response.js

--- blocks/slider_response.js
+++ blocks/slider_response.js
@@ -44,6 +44,7 @@ function GetPostLearnSlider(prompts, block_stimuli, task_name) {
 
   const createCanvasOnlyTrial = () => ({
     type: jsPsychCanvasKeyboardResponse,
+    canvas_size: [250,600],
     stimulus: async function (c) {
       const method = jsPsych.timelineVariable('method');
       const radius = jsPsych.timelineVariable('radius');
@@ -59,10 +60,31 @@ function GetPostLearnSlider(prompts, block_stimuli, task_name) {
   });
 
   const createSliderOnlyTrial = () => ({
-    type: jsPsychHtmlSliderResponse,
-    stimulus: '', // no image or canvas
-    prompt: jsPsych.timelineVariable('prompt'),
-    labels: jsPsych.timelineVariable('reflabel'),
+    type: jsPsychCanvasSliderResponse,
+    // stimulus: '', // no image or canvas
+    canvas_size: [80,600],
+    stimulus: async function(c) {
+      const method = 'SliderRef';
+      const radius = jsPsych.timelineVariable('radius');
+      const rand = jsPsych.timelineVariable('rand');
+      const condition = Math.random() < 0.5 ? 0 : 1; // governs reference order
+      await Morphfunction({ canvas: c, par: radius, rand: rand, condition: condition, method: method });
+      return c;
+    },
+    on_start: function() {
+      const container = jsPsych.getDisplayElement();
+      container.innerHTML = ''; // Clear previous content
+    },
+    on_load: function() { // Move prompt div below canvas, before buttons
+      const canvas = document.querySelector('canvas');
+      const prompt = document.createElement('div');
+      prompt.innerHTML = jsPsych.timelineVariable('prompt');
+      prompt.style.textAlign = 'center';
+      prompt.style.marginBottom = '50px';
+      canvas.insertAdjacentElement('beforebegin', prompt); // Insert the prompt after the canvas
+    },
+    prompt: `<p style="margin-Bottom: 2px !important;">Use the two reference objects, place the pink object on the scale.</p>`,
+    // labels: jsPsych.timelineVariable('reflabel'),
     require_movement: true,
     response_ends_trial: true,
     data: {
